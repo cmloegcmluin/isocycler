ad314db2e5af5522d4d1650225ac89eff19726fd
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2021-04-05 13:17:56 -0700

    just making it a bit more efficient

--- isocycler.js
+++ isocycler.js
@@ -1,16 +1,18 @@
 const edo = 12
 const steps = [...Array(edo).keys()]
 const sizes = steps.map(step => 2 ** -(step / edo))
+// TODO: Would be cool if you could also check JI pitches up to a certain odd limit or something (including e.g. 3/2)
 console.log(sizes)
 
-const maxTotalAbs = 5
+// TODO: would be cool if you could provide these as CLI args, though don't agonize over it because even moreso
+//  You should aspire to the web interface for it
+const maxTotalAbs = 6
 const vectorIsTooBad = vector => {
     const totalAbs = vector.reduce((totalAbs, term) => {
         return totalAbs + Math.abs(term)
     }, 0)
 
-    // console.log("total abs", totalAbs)
-    return totalAbs > maxTotalAbs
+    return totalAbs >= maxTotalAbs
 }
 
 const maxError = 0.001
@@ -22,52 +24,47 @@ const doCoolThingForVectorMaybeAddingSolution = vector => {
     })
 
     if (Math.abs(error) < maxError) {
-        solutions.push(vector)
+        solutions.push([vector, error])
     }
 }
 
-const checked = {}
-const doThingRecursivelyForVector = (vector) => {
-    // console.log("do for vector", vector)
-    const key = vector.toString()
-    if (checked[key]) {
-        // console.log('already hceckd', key)
-        return
-    }
-
-    checked[key] = true
+const doThingRecursivelyForVector = (vector, topIndex = 0) => {
     if (vectorIsTooBad(vector)) {
-        // console.log("bad")
         return
     }
 
     doCoolThingForVectorMaybeAddingSolution(vector)
 
-    vector.forEach((term, index) => {
+    // TODO: you could also increase the efficiency
+    //  by only searching for ones where the first non-zero term of the vector is positive
+    //  Effectively halving the space you search, since half are redundant
+    //  (and that's more efficient than waiting to see if the error is negative)
+    //  Which by the way you should at the end, if the error is negative,
+    //  Flip sign on all vector terms and the error to make everything consistent
+    for (let index = topIndex; index < vector.length; index++) {
         if (vector[index] === 0) {
             const newVector = [...vector]
             newVector[index] = newVector[index] + 1
-            doThingRecursivelyForVector(newVector)
+            doThingRecursivelyForVector(newVector, index)
 
             const newVector2 = [...vector]
             newVector2[index] = newVector2[index] - 1
-            doThingRecursivelyForVector(newVector2)
+            doThingRecursivelyForVector(newVector2, index)
         } else if (vector[index] > 0) {
             const newVector = [...vector]
             newVector[index] = newVector[index] + 1
-            doThingRecursivelyForVector(newVector)
+            doThingRecursivelyForVector(newVector, index)
         } else {
             const newVector2 = [...vector]
             newVector2[index] = newVector2[index] - 1
-            doThingRecursivelyForVector(newVector2)
+            doThingRecursivelyForVector(newVector2, index)
         }
-    })
+    }
 }
 
 const initialVector = [...Array(edo).keys()].map(_ => 0)
 doThingRecursivelyForVector(initialVector)
 
-// console.log(Object.keys(checked))
 solutions.forEach(solution => {
     console.log(solution.toString())
 })
